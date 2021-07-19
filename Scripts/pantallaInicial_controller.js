bcc5c977bbe585f90169281d5357624fcce710d4
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2021-07-19 13:53:12 -0600

    cambios a la pantalla principal, ayuda y musica

---
+++ Scripts/pantallaInicial_controller.js
@@ -0,0 +1,26 @@
+const audio =  new Audio('../Sounds/music_8bitJam.wav');  /*document.querySelector("audio");*/
+const icon = document.querySelector("#btn_music > i");
+const button = document.querySelector("#btn_music");
+audio.volume = 0.2;
+audio.loop = true;
+audio.play();
+    
+
+button.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.volume = 0.2;
+      audio.loop = true;
+      audio.play();
+      button.classList.add('fa-volume-up');
+      button.classList.remove('fa-volume-mute');
+      button.classList.add("fade");
+      
+      
+    } else {
+      audio.pause();
+      button.classList.add('fa-volume-mute');
+      button.classList.remove('fa-volume-up');
+      button.classList.remove("fade");
+    }    
+    
+  });
