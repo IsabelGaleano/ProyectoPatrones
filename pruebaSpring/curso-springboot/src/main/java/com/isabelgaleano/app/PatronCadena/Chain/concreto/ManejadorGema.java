41d941619a516f252e062286efe165be5c67e80d
IsaiasArce <isaiasarcerodriguez@gmail.com>
2021-07-22 16:39:33 -0600

    Update ManejadorGema.java

--- pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/PatronCadena/Chain/concreto/ManejadorGema.java
+++ pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/PatronCadena/Chain/concreto/ManejadorGema.java
@@ -8,6 +8,30 @@ public class ManejadorGema extends Manejador {
 
     @Override
     public void manejar(Personaje personaje, Casilla casilla) {
+        if(casilla.getTipo().equalsIgnoreCase("GemaVerde")){
+            if(personaje.getCantOro()+3>personaje.getmaxOro()){
+                personaje.setCantOro(personaje.getmaxOro());
+            }
+            personaje.setCantOro(personaje.getCantOro()+3);
 
+
+
+        }else if(casilla.getTipo().equalsIgnoreCase("GemaAzul")){
+            if(personaje.getCantOro()+3>personaje.getmaxOro()) {
+                personaje.setCantOro(personaje.getmaxOro());
+            }
+            personaje.setCantOro(personaje.getCantOro()+2);
+
+        }else if(casilla.getTipo().equalsIgnoreCase("GemaBlanca")){
+            if(personaje.getCantOro()+3>personaje.getmaxOro()) {
+                personaje.setCantOro(personaje.getmaxOro());
+            }
+            personaje.setCantOro(personaje.getCantOro()+2);
+
+        }
+        else{
+        getNextInChain().manejar(personaje, casilla);}
     }
+
+
 }
