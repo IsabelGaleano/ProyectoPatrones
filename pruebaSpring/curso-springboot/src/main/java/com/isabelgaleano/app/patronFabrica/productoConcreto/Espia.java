a0fbeb5d23b80b479c557ca5f644461964286a4e
Isabel Galeano <isagaleano9@gmail.com>
2021-07-22 22:57:38 -0600

    Add cambios patron visitante

--- pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/patronFabrica/productoConcreto/Espia.java
+++ pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/patronFabrica/productoConcreto/Espia.java
@@ -1,16 +1,18 @@
 package com.isabelgaleano.app.patronFabrica.productoConcreto;
 
 import com.isabelgaleano.app.patronFabrica.productoAbstracto.Personaje;
+import com.isabelgaleano.app.patronVisitante.IVisitor;
 
 public class Espia extends Personaje {
+
     private boolean visibilidad;
 
     public Espia(boolean visibilidad) {
         this.visibilidad = visibilidad;
     }
 
-    public Espia(int id, int precio, int vida, int cantMovimientos, int defensa, Ataque ataque, int cantOro, int maxOro, boolean visibilidad) {
-        super(id, precio, vida, cantMovimientos, defensa, ataque, cantOro, maxOro);
+    public Espia(int id, int precio, int vida, int cantMovimientos, int defensa, Ataque ataque, int cantOro, int maxOro, String estado, boolean visibilidad) {
+        super(id, precio, vida, cantMovimientos, defensa, ataque, cantOro, maxOro, estado);
         this.visibilidad = visibilidad;
     }
 
@@ -19,6 +21,11 @@ public class Espia extends Personaje {
     }
 
     public void setVisibilidad(boolean visibilidad) {
-        this.visibilidad = visibilidad;
+        this.visibilidad = visibilidad;}
+
+
+    @Override
+    public void aceptar(IVisitor iVisitor) {
+
     }
 }
