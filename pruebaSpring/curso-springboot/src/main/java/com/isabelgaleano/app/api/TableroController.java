dbeddfeba668a3e612425041a572c48fc72164c3
Isabel Galeano <isagaleano9@gmail.com>
2021-07-26 20:22:13 -0600

    ADD PASO DEL TABLERO AL FRONTEND

--- pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/api/TableroController.java
+++ pruebaSpring/curso-springboot/src/main/java/com/isabelgaleano/app/api/TableroController.java
@@ -1,28 +1,41 @@
 package com.isabelgaleano.app.api;
-
-
-import com.isabelgaleano.app.Castillo.Castillo;
 import com.isabelgaleano.app.Gestores.GestorTablero;
+import com.isabelgaleano.app.Modelo.Jugadores;
 import com.isabelgaleano.app.Tablero.Tablero;
-import org.springframework.http.HttpStatus;
+import com.isabelgaleano.app.service.JugadorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/tablero")
 public class TableroController {
-    private GestorTablero gestorTablero;
-
+    private GestorTablero gestorTablero = new GestorTablero();
+    @Autowired
+    private JugadorService jugadorService;
 
     @CrossOrigin
-    @GetMapping("/crearTablero")
-    public ResponseEntity<Tablero> crearTablero(@RequestBody ArrayList<JugadorObject> jugadores, ArrayList<Castillo> castillos) {
+    @PostMapping("/crearTablero")
+    public ResponseEntity<Tablero> crearTablero(@RequestBody Jugadores jugadores) {
+        ArrayList<JugadorObject> jugadoresArr = new ArrayList<JugadorObject>();
 
-        Tablero tablero = gestorTablero.crearTablero(jugadores, castillos);
+        String[] elements = jugadores.getJugadores().split(",");
+
+        for (String p: Arrays.asList(elements)) {
+            Optional<JugadorObject> jugador = jugadorService.findByAlias(p);
+            if (jugador.isPresent()) {
+                jugadoresArr.add(jugador.get());
+            }
+        }
+
+        Tablero tablero = gestorTablero.crearTablero(jugadoresArr);
+        if (tablero == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok().body(tablero);
 
     }
