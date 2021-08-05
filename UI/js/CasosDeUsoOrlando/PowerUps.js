692b159e3c3a29bd2b6246381b2b4c3af8daba0a
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2021-08-05 12:03:31 -0600

    cambios a compra de personajes con overlays

--- UI/js/CasosDeUsoOrlando/PowerUps.js
+++ UI/js/CasosDeUsoOrlando/PowerUps.js
@@ -1,4 +1,4 @@
-let elementsArray = document.querySelectorAll(".btn-compra-personaje");
+let elementsArray = document.querySelectorAll("#btn-comprar-OK");
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 //COMPRA DE LOS PERSONAJES DE LA TIENDA
